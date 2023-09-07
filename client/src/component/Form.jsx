5d9a651c78d959642cac3ff16e601444124cf137
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-09-07 21:35:26 +0500

    frontend data go to backend console

--- client/src/component/Form.jsx
+++ client/src/component/Form.jsx
@@ -1,73 +1,82 @@
-import React, { useRef, useState } from 'react'
-import Card from "./Card"
-import axios from "axios"
+import React, { useRef, useState } from 'react';
+import Card from './Card';
+import axios from 'axios';
 
 function Form() {
-    const  [FromData , setFormData] = useState([])
-    const titleRef = useRef("")
-    const descriptionRef = useRef("") 
-    const baseURL = " http://localhost:3000"
-    const formData = async(event)=>{
-    
-        event.preventDefault()
-        try{
-          const res = await axios.post(`${baseURL}/api/v1/postStory`)
-          console.log(res)
-          console.log("Called")
+  const [formData, setFormData] = useState([]);
+  const titleRef = useRef('');
+  const descriptionRef = useRef('');
+  const baseURL = 'http://localhost:8080';
 
-        }catch(e){
-          console.log(e)
-        }
-        // console.log(titleRef?.current?.value)
-        // console.log(titleRef?.current?.descriptionRef)
-        let title = titleRef?.current?.value 
-        let desc = descriptionRef?.current?.value
-        setFormData((previosData)=> [...previosData , {title , desc}  ])
-        console.log("Form Data")
-        console.log(FromData)
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const title = titleRef.current.value;
+    const desc = descriptionRef.current.value;
+
+    try {
+      const res = await axios.post(`${baseURL}/api/v1/postStory`, {
+        title: title,
+        desc: desc,
+      })
+      setFormData((previousData) => [...previousData, { title, desc }]);
+      console.log('Form Data');
+      console.log(formData);
+      event.target.reset();
+      // console.log(res.data); // Assuming the server returns data
+      console.log('Called');
+      console.log(res)
+    } catch (error) {
+      console.error(error);
     }
-    
-    return (<>
-     
-     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 className="text-2xl font-semibold mb-4">Create a Post</h1>
-        <form onSubmit={formData}>
-          <div className="mb-4">
-            <label htmlFor="title" className="block text-gray-700">Title</label>
-            <input
-              type="text"
-              id="title"
-              placeholder="Enter Your title"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-             ref={titleRef}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="description" className="block text-gray-700">Description</label>
-            <input
-              type="text"
-              id="description"
-              placeholder="Enter your description"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-              ref={descriptionRef}
-            
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:text-white text-black py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
-          >
-            Post
-          </button>
-        </form>
+
+ 
+  };
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+          <h1 className="text-2xl font-semibold mb-4">Create a Post</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="title" className="block text-gray-700">
+                Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                placeholder="Enter Your title"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                ref={titleRef}
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="description" className="block text-gray-700">
+                Description
+              </label>
+              <input
+                type="text"
+                id="description"
+                placeholder="Enter your description"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                ref={descriptionRef}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:text-white text-black py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+            >
+              Post
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-   <Card data = {FromData}></Card>
+      <Card data={formData}></Card>
     </>
-    )
+  );
 }
 
-export default Form
+export default Form;
--- index.mjs
+++ index.mjs
@@ -1,12 +1,26 @@
 import express from "express";
 import cors from "cors";
 import path from 'path';
+import { customAlphabet } from 'nanoid'
+const nanoid = customAlphabet('1234567890', 20)
+import { PineconeClient } from "@pinecone-database/pinecone";
+import OpenAI from "openai";
 const __dirname = path.resolve();
 import { config } from "dotenv";
 config();
-
+const BaseURL = "http://localhost:8080" ;
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const pinecone = new PineconeClient();
+await pinecone.init({
+  environment: process.env.PINECONE_ENVIRONMENT,
+  apiKey: process.env.PINECONE_API_KEY,
+});
 
 app.use(cors());
 
@@ -15,9 +29,10 @@ app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "./client/build")));
 
 // Create a product
-app.post("/api/v1/postStory", async (req, res) => {
+app.post(`/api/v1/postStory`, async (req, res) => {
   console.log("Product created function");
-
+  console.log(req.body)
+  res.send("HEllow")
 });
 
 // Get all products
