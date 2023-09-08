3775d5f6ea400d7bbca5275dd6fa1f2f1e116924
Sufyan Ahmed <sufiyan033130@gmail.com>
2023-09-08 05:19:34 +0500

    server get and post working well

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import OpenAI from "openai";
 const __dirname = path.resolve();
 import { config } from "dotenv";
 config();
-const BaseURL = "http://localhost:8080" ;
+const BaseURL = "http://localhost:8080";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -30,15 +30,76 @@ app.use("/", express.static(path.join(__dirname, "./client/build")));
 
 // Create a product
 app.post(`/api/v1/postStory`, async (req, res) => {
-  console.log("Product created function");
+
   console.log(req.body)
-  res.send("HEllow")
+  const response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: `${req.body?.title} ${req.body?.desc}`,
+  });
+  console.log("response?.data: ", response?.data);
+  const vector = response?.data[0]?.embedding
+  console.log("vector: ", vector);
+
+  const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+  const upsertRequest = {
+    vectors: [
+      {
+        id: nanoid(), // unique id, // unique id
+        values: vector,
+        metadata: {
+          title: req.body?.title,
+          body: req.body?.desc,
+        }
+      }
+    ],
+    // namespace: process.env.PINECONE_NAME_SPACE,
+  }
+  try {
+    const upsertResponse = await index.upsert({ upsertRequest });
+    console.log("upsertResponse: ", upsertResponse);
+
+    res.send({
+      message: "story created successfully"
+    });
+  } catch (e) {
+    console.log("error: ", e)
+    res.status(500).send({
+      message: "failed to create story, please try later"
+    });
+  }
 });
 
 // Get all products
-app.get("/check", async (req, res) => {
-  console.log("Get all products");
+app.get("/api/v1/allpost", async (req, res) => {
+  console.log("Get run ");
+  const queryText = ""
 
+
+  const response = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: queryText,
+  });
+  const vector = response?.data[0]?.embedding
+  console.log("vector: ", vector);
+  const index = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+  const queryResponse = await index.query({
+    queryRequest: {
+      vector: vector,
+      // id: "vec1",
+      topK: 100,
+      includeValues: true,
+      includeMetadata: true,
+      // namespace: process.env.PINECONE_NAME_SPACE
+    }
+  });
+
+  queryResponse.matches.map(eachMatch => {
+    console.log(`score ${eachMatch.score.toFixed(1)} => ${JSON.stringify(eachMatch.metadata)}\n\n`);
+  })
+  console.log(`${queryResponse.matches.length} records found `);
+
+  res.send(queryResponse.matches)
+  console.log("Donw", queryResponse.matches)
 });
 
 
